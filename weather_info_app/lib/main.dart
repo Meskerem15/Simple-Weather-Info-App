d37eeed2b41353241eb188c4fdf20ad20dc0778e
Meskerem Haile <meskyhaile24@gmail.com>
2024-09-25 19:54:55 -0400

    Basic UI with input field and button

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(WeatherInfoApp());
+  runApp(MyApp());
 }
 
-class WeatherInfoApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather Info App',
       home: Scaffold(
-        appBar: AppBar(title: Text('Weather Info')),
-        body: Center(child: Text('Weather Info Home')),
+        appBar: AppBar(
+          title: Text('Weather Info App'),
+        ),
+        body: WeatherInfo(),
+      ),
+    );
+  }
+}
+
+class WeatherInfo extends StatefulWidget {
+  @override
+  _WeatherInfoState createState() => _WeatherInfoState();
+}
+
+class _WeatherInfoState extends State<WeatherInfo> {
+  final TextEditingController _cityController = TextEditingController();
+  String _cityName = '';
+  String _temperature = '';
+  String _weatherCondition = '';
+
+  void fetchWeather() {
+    // To be implemented in the next steps
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _cityController,
+            decoration: InputDecoration(labelText: 'Enter city name'),
+          ),
+          ElevatedButton(
+            onPressed: fetchWeather,
+            child: Text('Fetch Weather'),
+          ),
+          SizedBox(height: 20),
+          Text('City: $_cityName'),
+          Text('Temperature: $_temperature'),
+          Text('Weather Condition: $_weatherCondition'),
+        ],
       ),
     );
   }
